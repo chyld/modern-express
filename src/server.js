eba5409c8fe5c4241cb661bfe62c1eea6aa13623
Chyld Medford <chyld.medford@gmail.com>
2016-06-08 15:28:17 -0700

    fix lint

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import winston from 'winston';
 
-const transport = new winston.transports.Console({level: process.env.LEVEL, colorize: true});
+const transport = new winston.transports.Console({ level: process.env.LEVEL, colorize: true });
 const logger = new (winston.Logger)({
-  transports: [transport]
+  transports: [transport],
 });
 
 const app = express();
@@ -16,12 +16,12 @@ const app = express();
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT, () => {
   logger.log('info', '[LISTENING] - port: %d', process.env.PORT);
 });
 
 app.get('/hello', (req, res) => {
-  res.send({payload: 'world'});
+  res.send({ payload: 'world' });
 });
